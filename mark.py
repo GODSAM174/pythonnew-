050e8573c11a996ee602b862130f56ebfd805b79
GODSAM174 <sanjeevmithrang@gmail.com>
2025-04-23 20:33:16 +0530

    Add files via upload

---
+++ mark.py
@@ -0,0 +1,15 @@
+# take marks as input from user
+print("marks obtained in 4 subjects: ")
+match=int(input("maths :"))
+english=int(input("english :"))
+science=int (input("science :"))
+hindi=int(input("hindi :" ))
+
+#let's calculate the percentage of marks
+# sum = maths+english+science+hindi
+# print("sum of maths,english,science,hindi") 
+
+percantage = (sum/400)*100
+
+print(end="percantage mark =")
+print(percantage)
