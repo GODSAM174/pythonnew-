b647a33570b0382fecbb001ec1332b3bd5b2fd5f
GODSAM174 <sanjeevmithrang@gmail.com>
2025-05-07 18:55:19 +0530

    Add files via upload

---
+++ operaters2.py
@@ -0,0 +1,29 @@
+a=10
+b=12
+c=0
+
+if a and b and c:
+    print("all the numbers have boolian value as true")
+else:
+    print("at least one number has boolian value as false")
+
+a=19
+b=-12
+c=0
+
+if a > 0 or b > 0:
+
+   print("either of the number is greater than 0")
+
+if b > 0 or c > 0:
+
+    print("either of number is greater than 0")
+
+else:
+    print("no number is greater than 0")
+
+if b > 0 or c > 0:
+    print("either of the number is greater than 0")
+
+else:
+    print("no number is greater than 0")
